54d6e54a0018d4871f8fd66ca054f2202f08ae41
amenir88 <ameni.rommene@esprit.tn>
2023-10-31 16:33:57 +0100

    temlate driven form

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -5,12 +5,14 @@ import { UserRoutingModule } from './user-routing.module';
 import { AccountsComponent } from './components/accounts/accounts.component';
 import { ListUserComponent } from './components/list-user/list-user.component';
 import { FormsModule } from '@angular/forms';
+import { AddUserComponent } from './components/add-user/add-user.component';
 
 
 @NgModule({
   declarations: [
     AccountsComponent,
-    ListUserComponent
+    ListUserComponent,
+    AddUserComponent
   ],
   imports: [
     CommonModule,
